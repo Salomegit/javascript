1e3c833f63b6de644569a7c2301341aec7050e41
salome <sallygithinji72@gmail.com>
2023-05-30 11:16:24 +0300

    basics javascript

---
+++ main.js
@@ -0,0 +1,25 @@
+// console.log("Hello form the other");
+// console.log("Hello form the other");
+
+// //variable
+// let age = 20;
+// console.log(age);
+// const salary = 2244
+// console.log(salary);
+const name="Salome"
+const channel="was herer"   
+console.log(name);
+let age
+console.log(age);
+const person ={
+    'firstname':'Bruce',
+    'lastName':'Wayne',
+    'age':'30', 
+}
+console.log(person.firstname);
+const evenNumbers = [2,4,6,,8,10];
+console.log(evenNumbers[0]);
+
+let a = 12;
+let b = 14;
+console.log(7 < a && 10 < b)

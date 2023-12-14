ca2abd31a5bd8a524add1f90dd092eeb082e0713
salome <sallygithinji72@gmail.com>
2023-12-14 22:23:35 +0300

    learning about promises

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
-// console.log("Hello form the other");
-// console.log("Hello form the other");
+let p = new Promise((resolve, reject) => {
+    let x = 1 - 2
+    x == -1? resolve("Success") : reject("failure")
+})
 
-// //variable
-// let age = 20;
-// console.log(age);
-// const salary = 2244
-// console.log(salary);
-const name="Salome"
-const channel="was herer"   
-console.log(name);
-let age
-console.log(age);
-const person ={
-    'firstname':'Bruce',
-    'lastName':'Wayne',
-    'age':'30', 
-}
-console.log(person.firstname);
-const evenNumbers = [2,4,6,,8,10];
-console.log(evenNumbers[0]);
 
-let a = 12;
-let b = 14;
-console.log(7 < a && 10 < b)
+p.then(message => {
+    console.log(
+        "This is in the then" + message
+    );
+})
+p.catch(message => {
+    console.log("This is in the catch" + message)
+})
+ 

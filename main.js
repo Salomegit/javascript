c4fb3f56e96417bcbaf438cf8ec5c69f787523d4
salome <sallygithinji72@gmail.com>
2024-01-10 22:51:12 +0300

    async and await

--- main.js
+++ main.js
@@ -258,10 +258,21 @@
 
 
 
-    console.log(
       
 
-      fetch('https://jsonplaceholder.typicode.com/todos') 
-      .then(console.log((res)=>res.json()))
+    //   fetch('https://jsonplaceholder.typicode.com/todos') 
+    //   .then((res)=>res.json())
+    //   .then((data)=>{console.log(data)})
     
-    )
+    
+    // console.log("love")
+
+
+
+      async function getInfo() {
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos') 
+        const data = await res.json();
+        console.log(data)
+      }
+
+      getInfo();
